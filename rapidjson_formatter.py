bf56e0f8db543ccb8667fcd307d94f021b5f14eb
Tim Lindeberg <tim.lindeberg@gmail.com>
2019-03-16 13:28:15 +0100

    Cleaned up code and added more nested tests

--- rapidjson_formatter.py
+++ rapidjson_formatter.py
@@ -1,46 +1,151 @@
 from __future__ import print_function
 import lldb
 import lldb.formatters.Logger
-import sys
 import traceback
 
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
+# def eprint(*args, **kwargs):
+#     import sys
+#     print(*args, file=sys.stderr, **kwargs)
 
 
-kNullType = 0
-kFalseType = 1
-kTrueType = 2
-kObjectType = 3
-kArrayType = 4
-kStringType = 5
-kNumberType = 6
+def __lldb_init_module(debugger, dict):
+    debugger.HandleCommand(
+        'type summary add -F rapidjson_formatter.SummaryProvider -e -x "^rapidjson::GenericValue<.+>$" -w rapidjson')
+    debugger.HandleCommand(
+        'type synthetic add -l rapidjson_formatter.SynthProvider -x "^rapidjson::GenericValue<.+>$" -w rapidjson')
+    debugger.HandleCommand(
+        'type synthetic add -l rapidjson_formatter.SynthProvider -x "^rapidjson::Document$" -w rapidjson')
+    debugger.HandleCommand("type category enable rapidjson")
 
-kBoolFlag = 0x0008
-kNumberFlag = 0x0010
-kIntFlag = 0x0020
-kUintFlag = 0x0040
-kInt64Flag = 0x0080
-kUint64Flag = 0x0100
-kDoubleFlag = 0x0200
-kStringFlag = 0x0400
-kCopyFlag = 0x0800
-kInlineStrFlag = 0x1000
-kObjectFlag = kObjectType
-kArrayFlag = kArrayType
 
-kNullFlag = kNullType
-kTrueFlag = kTrueType | kBoolFlag
-kFalseFlag = kFalseType | kBoolFlag
-kNumberIntFlag = kNumberType | kNumberFlag | kIntFlag | kInt64Flag
-kNumberUintFlag = kNumberType | kNumberFlag | kUintFlag | kUint64Flag | kInt64Flag
-kNumberInt64Flag = kNumberType | kNumberFlag | kInt64Flag
-kNumberUint64Flag = kNumberType | kNumberFlag | kUint64Flag
-kNumberDoubleFlag = kNumberType | kNumberFlag | kDoubleFlag
-kNumberAnyFlag = kNumberType | kNumberFlag | kIntFlag | kInt64Flag | kUintFlag | kUint64Flag | kDoubleFlag
-kConstStringFlag = kStringType | kStringFlag
-kCopyStringFlag = kStringType | kStringFlag | kCopyFlag
-kShortStringFlag = kStringType | kStringFlag | kCopyFlag | kInlineStrFlag
+def SummaryProvider(valobj, dict):
+    synth = SynthProvider(valobj, dict)
+    synth.update()
+    return synth.get_summary()
+
+class SynthProvider:
+    def __init__(self, valobj, dict):
+        self.valobj = valobj
+
+        self.data = None
+        self.flags = None
+        self.type = self.valobj.GetType()
+        self._num_children = 0
+
+    def num_children(self):
+        return self._num_children
+
+    def get_child_index(self, name):
+        try:
+            return int(name[1:name.rfind("]")])
+        except:
+            traceback.print_exc()
+            return -1
+
+    def get_child_at_index(self, index):
+        try:
+            if index < 0:
+                return None
+            if index >= self._num_children:
+                return None
+
+            if self.flags == kArrayFlag:  return self._get_array_child(index)
+            if self.flags == kObjectFlag: return self._get_object_child(index)
+            return None
+        except:
+            traceback.print_exc()
+            return None
+
+    def update(self):
+        self.data = self._get_data_value()
+        self.flags = get_flags(self.data)
+        self._num_children = self._get_num_children()
+
+    def has_children(self):
+        return self._num_children > 0
+
+    def get_summary(self):
+        flags = self.flags
+
+        def get_number_object(type):
+            return self._get_object_on_data("n").GetChildMemberWithName(type)
+
+        def is_set(flag):
+            return is_flag_set(self.flags, flag)
+
+        try:
+            if flags == kNullFlag:     return "null"
+            if flags == kFalseFlag:    return "false"
+            if flags == kTrueFlag:     return "true"
+            if flags == kArrayFlag:    return "<Array> size=%s" % self._num_children
+            if flags == kObjectFlag:   return "<Object> size=%s" % self._num_children
+            if is_set(kStringFlag):    return get_string(flags, self.data)
+            if is_set(kDoubleFlag):    return get_number_object("d").GetValue()
+            if is_set(kUint64Flag):    return get_number_object("u64").GetValue()
+            if is_set(kInt64Flag):     return get_number_object("i64").GetValue()
+            if is_set(kUintFlag):      return get_number_object("u").GetChildMemberWithName("u").GetValue()
+            if is_set(kIntFlag):       return get_number_object("i").GetChildMemberWithName("i").GetValue()
+            return None
+        except:
+            traceback.print_exc()
+            return None
+
+    def _get_data_value(self):
+        # We read the address of the object and use that in CreateValueFromExpression
+        # to fetch the data_ object. This seems to be the only way to access data_
+        # once we've attached a synthetic children provider for this type
+        address = self.valobj.GetAddress().GetOffset()
+        expr = "reinterpret_cast<rapidjson::Value*>(%s)->data_" % address
+        return self.valobj.CreateValueFromExpression("data", expr)
+
+    def _get_num_children(self):
+        def get_type():
+            if self.flags == kArrayFlag:    return "a"
+            elif self.flags == kObjectFlag: return "o"
+            else:                           return ""
+        try:
+            return self._get_object_on_data(get_type()).GetChildMemberWithName("size").GetValueAsUnsigned()
+        except:
+            traceback.print_exc()
+            return 0
+
+    def _get_array_child(self, index):
+        arr = self._get_object_on_data("a")
+        start = arr.GetChildMemberWithName("elements")
+        offset = index * self.type.GetByteSize()
+        address = get_valid_address(start) + offset
+        return self.valobj.CreateValueFromAddress('[' + str(index) + ']', address, self.type)
+
+    def _get_object_child(self, index):
+        address = self._get_member_address(index)
+        masked_address = str(mask_address(address))
+        name = self._get_object_name(masked_address, index)
+
+        # We need to create a value form address, otherwise we won't
+        # be able to read the data_ object of the created child. As an added
+        # benefit, we can put the typedef rapidjson::Value as type instead of
+        # very long rapidjson::GenericValue<...>
+        expr = "&reinterpret_cast<rapidjson::Value::Member*>(%s)->value" % masked_address
+        obj_child = self.valobj.CreateValueFromExpression(name + "addr", expr)
+        return self.valobj.CreateValueFromAddress(name, get_pointer_adress(obj_child), self.type)
+
+    def _get_object_name(self, address, index):
+        expr = "reinterpret_cast<rapidjson::Value::Member*>(%s)->name->data_" % address
+        data_value = self.valobj.CreateValueFromExpression("name", expr)
+        flags = get_flags(data_value)
+        member_name = get_string(flags, data_value)
+        return '[%s] %s' % (index, member_name)
+
+    def _get_member_address(self, index):
+        obj = self._get_object_on_data("o")
+        start = obj.GetChildMemberWithName("members")
+
+        member_type = start.GetType().GetPointeeType()
+        offset = index * member_type.GetByteSize()
+        return get_valid_address(start) + offset
+
+    def _get_object_on_data(self, type):
+        return self.data.GetChildMemberWithName(type)
 
 
 def get_string_from_array(array):
@@ -79,171 +184,76 @@ def get_string_from_memory(starting_address):
         address += chunk_size
 
 
-def rapidjson_SummaryProvider(valobj, dict):
-    synth = rapidjson_SynthProvider(valobj, dict)
-    synth.update()
-    summary = synth.get_summary()
-    return summary
+def get_string(flags, data_value):
+    if is_flag_set(flags, kInlineStrFlag):
+        return get_inline_string(data_value)
+
+    s = data_value.GetChildMemberWithName("s")
+    str = s.GetChildMemberWithName("str")
+    address = get_valid_address(str)
+
+    return '"%s"' % get_string_from_memory(address)
 
 
-class rapidjson_SynthProvider:
-    def __init__(self, valobj, dict):
-        self.valobj = valobj
+def get_inline_string(data_value):
+    ss = data_value.GetChildMemberWithName("ss")
+    str = ss.GetChildMemberWithName("str")
 
-        self.data = None
-        self.flags = None
-        self.type = self.valobj.GetType()
-
-    def num_children(self):
-        eprint("num_children %s" % self.valobj.GetName())
-        try:
-            res = self._get_num_children()
-            return res
-        except:
-            traceback.print_exc()
-            return 0
-
-    def _get_num_children(self):
-        if self.flags == kArrayFlag:
-            data = self._get_data_object("a")
-            return data.GetChildMemberWithName("size").GetValueAsUnsigned()
-        if self.flags == kObjectFlag:
-            data = self._get_data_object("o")
-            return data.GetChildMemberWithName("size").GetValueAsUnsigned()
-        return 0
-
-    def get_child_index(self, name):
-        eprint("get_child_index(%s) %s" % (name, self.valobj.GetName()))
-        try:
-            return -1
-        except:
-            traceback.print_exc()
-            return -1
-
-    def get_child_at_index(self, index):
-        if index < 0:
-            return None
-        if index >= self.num_children():
-            return None
-
-        try:
-            if self.flags == kArrayFlag: return self._get_array_child(index)
-            if self.flags == kObjectFlag: return self._get_object_child(index)
-            return None
-        except:
-            traceback.print_exc()
-            return None
-
-    def _get_array_child(self, index):
-        arr = self._get_data_object("a")
-        start = arr.GetChildMemberWithName("elements")
-        offset = index * self.type.GetByteSize()
-        address = self._get_valid_address(start) + offset
-        return self.valobj.CreateValueFromAddress('[' + str(index) + ']', address, self.type)
-
-    def _get_object_child(self, index):
-        address = self._get_member_address(index)
-        masked_address = str(self._mask_address(address))
-        name = self._get_object_name(masked_address, index)
-
-        # We need to create a value form address, otherwise we won't
-        # be able to read the data_ object of the created child
-        obj_child = self.valobj.CreateValueFromExpression(name + "addr", "&reinterpret_cast<rapidjson::Value::Member*>(" + masked_address + ")->value")
-        child_address = int(obj_child.GetValue(), 16)
-        return self.valobj.CreateValueFromAddress(name, child_address, self.type)
-
-    def _get_object_name(self, address, index):
-        data_value = self.valobj.CreateValueFromExpression("name", "reinterpret_cast<rapidjson::Value::Member*>(" + address + ")->name->data_")
-        flags = self._get_flags(data_value)
-        is_inline = flags & kInlineStrFlag != 0
-        member_name = self._get_inline_string(data_value) if is_inline else self._get_string(data_value)
-        return '[%s] %s' % (index, member_name)
-
-    def _get_member_address(self, index):
-        obj = self._get_data_object("o")
-        start = obj.GetChildMemberWithName("members")
-
-        member_type = start.GetType().GetPointeeType()
-        offset = index * member_type.GetByteSize()
-        return self._get_valid_address(start) + offset
-
-    def update(self):
-        eprint("update %s" % self.valobj.GetName())
-
-        # We read the address of the object and use that in CreateValueFromExpression
-        # to fetch the data_ object. This seems to be the only way to access data_
-        # once we've attached a synthetic children provider to the same type
-        address = self.valobj.GetAddress().GetOffset()
-        self.data = self.valobj.CreateValueFromExpression("data", "reinterpret_cast<rapidjson::Value*>(" + str(address) + ")->data_")
-        self.flags = self._get_flags(self.data)
-
-    def has_children(self):
-        return self.flags == kArrayFlag or self.flags == kObjectFlag
-
-    def get_summary(self):
-        eprint("get_summary %s" % self.valobj.GetName())
-
-        try:
-            if self.flags == kNullFlag:      return "null"
-            if self.flags == kFalseFlag:     return "false"
-            if self.flags == kTrueFlag:      return "true"
-            if self.flags == kArrayFlag:     return "<Array> size=%s" % self.num_children()
-            if self.flags == kObjectFlag:    return "<Object> size=%s" % self.num_children()
-            if self._is_set(kInlineStrFlag): return self._get_inline_string(self.data)
-            if self._is_set(kStringFlag):    return self._get_string(self.data)
-            if self._is_set(kDoubleFlag):    return self._get_number_object("d").GetValue()
-            if self._is_set(kUint64Flag):    return self._get_number_object("u64").GetValue()
-            if self._is_set(kInt64Flag):     return self._get_number_object("i64").GetValue()
-            if self._is_set(kUintFlag):      return self._get_number_object("u").GetChildMemberWithName("u").GetValue()
-            if self._is_set(kIntFlag):       return self._get_number_object("i").GetChildMemberWithName("i").GetValue()
-            return None
-        except:
-            return None
-
-    def _is_set(self, flag):
-        return (self.flags & flag) != 0
-
-    def _get_data_object(self, type):
-        return self.data.GetChildMemberWithName(type)
-
-    def _get_number_object(self, type):
-        return self._get_data_object("n").GetChildMemberWithName(type)
-
-    def _get_flags(self, data_value):
-        f = data_value.GetChildMemberWithName("f")
-        flags = f.GetChildMemberWithName("flags")
-        return flags.GetValueAsUnsigned()
-
-    def _get_inline_string(self, data_value):
-        ss = data_value.GetChildMemberWithName("ss")
-        str = ss.GetChildMemberWithName("str")
-
-        return '"%s"' % get_string_from_array(str)
-
-    def _get_string(self, data_value):
-        s = data_value.GetChildMemberWithName("s")
-        str = s.GetChildMemberWithName("str")
-        address = self._get_valid_address(str)
-
-        return '"%s"' % get_string_from_memory(address)
-
-    def _get_pointer_adress(self, pointer_value):
-        return int(pointer_value.GetValue(), 16)
-
-    def _get_valid_address(self, obj):
-        return self._mask_address(self._get_pointer_adress(obj))
-
-    def _mask_address(self, address):
-        return address & 0x0000FFFFFFFFFFFF
+    return '"%s"' % get_string_from_array(str)
 
 
-def __lldb_init_module(debugger, dict):
-    debugger.HandleCommand(
-        'type summary add -F rapidjson_formatter.rapidjson_SummaryProvider -e -x "^rapidjson::GenericValue<.+>$" -w rapidjson')
-    debugger.HandleCommand(
-         'type synthetic add -l rapidjson_formatter.rapidjson_SynthProvider -x "^rapidjson::GenericValue<.+>$" -w rapidjson_synth')
-    debugger.HandleCommand(
-        'type synthetic add -l rapidjson_formatter.rapidjson_SynthProvider -x "^rapidjson::Document$" -w rapidjson_synth')
-    debugger.HandleCommand("type category enable rapidjson")
-    debugger.HandleCommand("type category enable rapidjson_synth")
+def is_flag_set(flags, flag):
+    return (flags & flag) != 0
 
+
+def get_flags(data_value):
+    f = data_value.GetChildMemberWithName("f")
+    flags = f.GetChildMemberWithName("flags")
+    return flags.GetValueAsUnsigned()
+
+
+def mask_address(address):
+    return address & 0x0000FFFFFFFFFFFF
+
+
+def get_pointer_adress(pointer_value):
+    return int(pointer_value.GetValue(), 16)
+
+
+def get_valid_address(obj):
+    return mask_address(get_pointer_adress(obj))
+
+
+kNullType = 0
+kFalseType = 1
+kTrueType = 2
+kObjectType = 3
+kArrayType = 4
+kStringType = 5
+kNumberType = 6
+
+kBoolFlag = 0x0008
+kNumberFlag = 0x0010
+kIntFlag = 0x0020
+kUintFlag = 0x0040
+kInt64Flag = 0x0080
+kUint64Flag = 0x0100
+kDoubleFlag = 0x0200
+kStringFlag = 0x0400
+kCopyFlag = 0x0800
+kInlineStrFlag = 0x1000
+kObjectFlag = kObjectType
+kArrayFlag = kArrayType
+
+kNullFlag = kNullType
+kTrueFlag = kTrueType | kBoolFlag
+kFalseFlag = kFalseType | kBoolFlag
+kNumberIntFlag = kNumberType | kNumberFlag | kIntFlag | kInt64Flag
+kNumberUintFlag = kNumberType | kNumberFlag | kUintFlag | kUint64Flag | kInt64Flag
+kNumberInt64Flag = kNumberType | kNumberFlag | kInt64Flag
+kNumberUint64Flag = kNumberType | kNumberFlag | kUint64Flag
+kNumberDoubleFlag = kNumberType | kNumberFlag | kDoubleFlag
+kNumberAnyFlag = kNumberType | kNumberFlag | kIntFlag | kInt64Flag | kUintFlag | kUint64Flag | kDoubleFlag
+kConstStringFlag = kStringType | kStringFlag
+kCopyStringFlag = kStringType | kStringFlag | kCopyFlag
+kShortStringFlag = kStringType | kStringFlag | kCopyFlag | kInlineStrFlag
---
+++ test.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include "rapidjson/document.h"
+
+int main() {
+    auto json = R"(
+    {
+        "int": 1,
+        "float": 2.0,
+        "true": true,
+        "false": false,
+        "null": null,
+        "array": [1, 2, 3],
+        "object": {
+            "A": 1,
+            "B": 2,
+            "C": 3
+        }
+    }
+    )";
+
+    rapidjson::Document doc;
+    doc.SetObject();
+    //doc.Parse(json);
+
+
+    rapidjson::Value t;
+    t.SetBool(true);
+    rapidjson::Value f;
+    f.SetBool(false);
+    rapidjson::Value n;
+    n.SetNull();
+    rapidjson::Value i;
+    i.SetInt(1);
+    rapidjson::Value u;
+    u.SetUint(2);
+    rapidjson::Value i64;
+    i64.SetInt64(-123456789012345LL);
+    rapidjson::Value u64;
+    u64.SetUint64(234567890123456LL);
+    rapidjson::Value fl;
+    fl.SetFloat(5.5);
+    rapidjson::Value d;
+    d.SetDouble(6.6);
+    rapidjson::Value s1;
+    s1.SetString("ABC");
+    rapidjson::Value s2;
+    s2.SetString(rapidjson::StringRef("ABC12345678910ABC12345678910ABC12345678910ABC12345678910ABC12345678910"));
+    rapidjson::Value s3;
+    s3.SetString("dhjkdashjkduqwdaskjhdnsajdqwdyaisdh", doc.GetAllocator());
+    rapidjson::Value s4;
+    s4.SetString("12345", 5);
+
+    rapidjson::Value arr1;
+    arr1.SetArray();
+    arr1.PushBack(t, doc.GetAllocator());
+    arr1.PushBack(f, doc.GetAllocator());
+    arr1.PushBack(n, doc.GetAllocator());
+
+    rapidjson::Value arr2;
+    arr2.SetArray();
+    arr2.PushBack(i, doc.GetAllocator());
+    arr2.PushBack(arr1, doc.GetAllocator());
+    arr2.PushBack(u64, doc.GetAllocator());
+
+    rapidjson::Value obj1;
+    obj1.SetObject();
+    obj1.AddMember(rapidjson::StringRef("myFloat"), fl, doc.GetAllocator());
+    obj1.AddMember(rapidjson::StringRef("myArray"), arr2, doc.GetAllocator());
+    obj1.AddMember(rapidjson::StringRef("myDouble"), d, doc.GetAllocator());
+
+    rapidjson::Value arr3;
+    arr3.SetArray();
+    arr3.PushBack(s1, doc.GetAllocator());
+    arr3.PushBack(s2, doc.GetAllocator());
+    arr3.PushBack(obj1, doc.GetAllocator());
+
+    rapidjson::Value obj2;
+    obj2.SetObject();
+    obj2.AddMember(rapidjson::StringRef("myUnsigned"), u, doc.GetAllocator());
+    obj2.AddMember(s3, s4, doc.GetAllocator());
+    obj2.AddMember(rapidjson::StringRef("myArray"), arr3, doc.GetAllocator());
+    obj2.AddMember(rapidjson::StringRef("myInt64"), i64, doc.GetAllocator());
+
+    return 0;
+}
